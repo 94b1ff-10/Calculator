14cbc7cea0bee6839834e60a925b644d5cccd079
No.10 <maruten007@gmail.com>
2022-10-01 00:40:10 +0900

    just saved current progress.

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//設計図
+class CalculateSimulator {
+    
+//    使用する対象
+    var first: Double = 0
+    var second: Double = 0
+    
+//    実行処理
+    func result {
+        first
+    }
+    
+}
+
+
+
+let calculate = CalculateSimulator()
+
+
+print(5+5)
